8efdb599ef95c1c42aa49cc8fff92b0a42b0d907
Manik Dingra <manikdhingra0582@gmail.com>
2023-01-09 23:42:37 +0530

    CSS is used instead of material UI

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
-
+import React from "react";
+import "./styles.css";
+import SearchIcon from "@mui/icons-material/Search";
 const Header = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	return (
+		<div className="header">
+			<h1>Travel Advisor</h1>
+			<div className="searchBar">
+				<h1>Explore New Places</h1>
+				<SearchIcon id="query" />
+				<input type="search" id="query" name="q" placeholder="Search..." />
+			</div>
+		</div>
+	);
+};
 
-export default Header
+export default Header;
