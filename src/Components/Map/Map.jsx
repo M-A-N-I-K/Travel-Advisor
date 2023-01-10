c0deb77c1e8402f4f03d83071b7a3565f1ed7714
Manik Dingra <manikdhingra0582@gmail.com>
2023-01-10 23:52:04 +0530

    Google maps api issue generated

--- src/Components/Map/Map.jsx
+++ src/Components/Map/Map.jsx
@@ -1,11 +1,23 @@
-import React from 'react'
-
+import React from "react";
+import GoogleMapReact from "google-map-react";
 const Map = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+	const coordinates = { lat: 0, lng: 0 };
+	return (
+		<div className="map">
+			<GoogleMapReact
+				bootstrapURLKeys={{
+					key: "AIzaSyA3VMjWK06KCqs_wiiLa0bWowWmRIYs8Ds",
+				}}
+				defaultCenter={coordinates}
+				center={coordinates}
+				defaultZoom={14}
+				margin={[50, 50, 50, 50]}
+				options={""}
+				onChange={""}
+				onChildClick={""}
+			></GoogleMapReact>
+		</div>
+	);
+};
 
-export default Map
+export default Map;
